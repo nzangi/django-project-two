2a200a9637615645511fa6693ae007cb4145d4dd
nzangi <nzangimuoki284@gmail.com>
2023-10-16 15:45:25 +0300

    Itmes Lista added

--- item/views.py
+++ item/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_list_or_404
+
+from .models import Item
 
 # Create your views here.
+def detail(request,pk):
+    item = get_list_or_404(Item,pk)
+    return render(request,'item/detail.html',{
+        'item': item
+    })
