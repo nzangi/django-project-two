9014f982666a9d7b58ac34bbc03b970f579f8ca8
nzangi <nzangimuoki284@gmail.com>
2023-10-19 16:01:38 +0300

    Related Items  added

--- core/views.py
+++ core/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
-from item.models import Category,Item
+from item.models import Category, Item
+
+
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
 
-    return render(request, "core/index.html",{
-        'categories':categories,
-        'items':items,
+    return render(request, "core/index.html", {
+        'categories': categories,
+        'items': items,
 
     })
 
+
 def contact(request):
-    return render(request,'core/contact.html')
+    return render(request, 'core/contact.html')
--- item/urls.py
+++ item/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
 from . import views
+
 app_name = 'item'
 urlpatterns = [
-    path('<int:pk>/',views.detail,name='detail')
-    
-]
+    path('<int:pk>/', views.detail, name='detail')
 
+]
