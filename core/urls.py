04a1e167cda6af637cb234d4c9e441afab992cba
nzangi <nzangimuoki284@gmail.com>
2023-10-24 16:55:37 +0300

    Login Page added

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,12 @@
 from . import views
+from django.contrib.auth import views as auth_views
 from django.urls import path
+from .forms import LoginForm
 app_name='core'
 urlpatterns = [
     path('',views.index,name='index'),
     path('signup/',views.signup,name='signup'),
     path('contact/', views.contact, name='contact'),
+    path('login/',auth_views.LoginView.as_view(template_name='core/login.html',authentication_form= LoginForm),name='login')
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 from item.models import Category, Item
@@ -21,7 +21,14 @@ def contact(request):
 
 
 def signup(request):
-    form = SignUpForm()
-    return render(request,'core/signup.html',{
-        'form' : form
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form = SignUpForm()
+    return render(request, 'core/signup.html', {
+        'form': form
     })
